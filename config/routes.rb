d57f59664b0145b5167375a7066532a795801621
zhenming <ongzhenming@gmail.com>
2013-07-02 17:30:35 +0800

    Finish layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Asksg::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/about',  to: 'static_pages#about'
+  match '/signup',  to: 'users#new'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,4 +17,14 @@ describe "Static Pages" do
     it { should have_selector('title', text: "Ask SG | About") }
     it { should have_selector('h1',    text: "About") }
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Sign up"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "ask sg"
+    page.should have_selector 'h1', text: "Ask SG"
+  end  
 end
